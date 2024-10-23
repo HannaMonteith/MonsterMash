f67f5cdfea181214daccadf59842ef90c8fab410
HannaMonteith <hmonteit@uvm.edu>
2024-10-23 12:03:54 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log()
+console.log("Monsters!");
+
+function Monster(tails, tenctales, eyes, horns) {
+    this.tails = tails;
+    this.tenctales = tenctales;
+    this.eyes  = eyes;
+    this.horns = horns;
+}
